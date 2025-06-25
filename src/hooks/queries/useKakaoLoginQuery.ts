df95ebc2c68cef7a35b9ec99fcee8feddb20da11
Jina <jinatye@naver.com>
2025-06-25 16:58:47 +0900

    [api] : 카카오로그인 연동 완료

--- src/hooks/queries/useKakaoLoginQuery.ts
+++ src/hooks/queries/useKakaoLoginQuery.ts
@@ -1,10 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import { kakaoLogin } from "../../api/user";
 
-export const useKakaoLoginQuery = (code: string) => {
+interface KakaoLoginParams {
+  code: string;
+  redirectUri: string;
+}
+
+export const useKakaoLoginQuery = ({ code, redirectUri }: KakaoLoginParams) => {
   return useQuery({
-    queryKey: ["kakaoLogin", code],
-    queryFn: () => kakaoLogin(code),
-    enabled: !!code,
+    queryKey: ["kakao-login", code],
+    queryFn: () => kakaoLogin(redirectUri, code),
+    enabled: !!code && !!redirectUri, // ✅ code와 url이 다 있어야 실행
   });
 };
